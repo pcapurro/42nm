caf49b039af4083397cc9f1b76efc2d67dc0aa9e
pcapurro <fossa634@gmail.com>
2025-01-30 07:17:56 +0100

    isUndefinedWeak() added, isReadMode(), isWeakNormal() and isWeakUnknown() rewrote, isGlobal() added to replace isLocal()

--- srcs/heart/symbols/type.c
+++ srcs/heart/symbols/type.c
@@ -122,7 +122,7 @@ static bool	isNote(const char* binary, tSymbols* symbol, tStrs* strs, const int 
 
 		if (i == data->st_shndx)
 		{
-			if (section->sh_type == SHT_NOTE)
+			if (section->sh_type == SHT_NOTE && (section->sh_flags & SHF_WRITE) == 0)
 				return (true);
 
 			return (false);
@@ -188,11 +188,24 @@ static bool	isText(const char* binary, tSymbols* symbol, tStrs* strs, const int 
 	return (false);
 }
 
+static bool	isUndefinedWeak(tSymbols* symbol, tStrs* strs, const int value)
+{
+	Elf64_Sym*	data = symbol->data;
+
+	if (ELF64_ST_BIND(data->st_info) != STB_GLOBAL)
+		return (false);
+
+	if (data->st_shndx == SHN_UNDEF && ELF64_ST_BIND(data->st_info) == STB_WEAK)
+		return (true);
+
+	return (false);
+}
+
 static bool	isUndefined(tSymbols* symbol, tStrs* strs, const int value)
 {
 	Elf64_Sym*	data = symbol->data;
 
-	if (data->st_shndx == SHN_UNDEF)
+	if (data->st_shndx == SHN_UNDEF && ELF64_ST_BIND(data->st_info) == STB_GLOBAL)
 		return (true);
 
 	return (false);
@@ -202,7 +215,7 @@ static bool	isWeakNormal(tSymbols* symbol, tStrs* strs, const int value)
 {
 	Elf64_Sym*	data = symbol->data;
 
-	if (ELF64_ST_BIND(data->st_info) == STB_WEAK)
+	if (ELF64_ST_TYPE(data->st_info) == STT_OBJECT && ELF64_ST_BIND(data->st_info) == STB_WEAK)
 		return (true);
 
 	return (false);
@@ -212,7 +225,7 @@ static bool	isWeakUnknown(tSymbols* symbol, tStrs* strs, const int value)
 {
 	Elf64_Sym*	data = symbol->data;
 
-	if (ELF64_ST_BIND(data->st_info) == STB_WEAK)
+	if (ELF64_ST_TYPE(data->st_info) != STT_OBJECT && ELF64_ST_BIND(data->st_info) == STB_WEAK)
 		return (true);
 
 	return (false);
@@ -232,11 +245,12 @@ static bool isLocalOrGlobal(const char type)
 	return (true);
 }
 
-static bool	isLocal(tSymbols* symbol, tStrs* strs, const int value)
+static bool	isGlobal(tSymbols* symbol, tStrs* strs, const int value)
 {
 	Elf64_Sym*	data = symbol->data;
 
-	if (ELF64_ST_BIND(data->st_info) == STB_LOCAL)
+	if (ELF64_ST_BIND(data->st_info) == STB_GLOBAL \
+		|| (ELF64_ST_BIND(data->st_info) == STB_WEAK && data->st_shndx != SHN_UNDEF))
 		return (true);
 
 	return (false);
@@ -258,10 +272,6 @@ char*	getType(const char* binary, tSymbols* symbol, tStrs* strs, const int value
 
 	else if (isBSS(binary, symbol, strs, value) == true)
 		type[0] = 'B'; // v
-	// the symbol is in the '.bss' data section (= a section of uninitialized data) (SHT_NOBITS type)
-	// .bss data section:
-	// = commonly used for static/global variables uninitialized or set to zero
-	// > doesn't contain any data in the binary file, space being used during execution
 
 	else if (isCommon(symbol, strs, value) == true)
 		type[0] = 'C'; // v
@@ -272,20 +282,20 @@ char*	getType(const char* binary, tSymbols* symbol, tStrs* strs, const int value
 	else if (isIndirect(symbol, strs, value) == true)
 		type[0] = 'i'; // v
 
+	else if (isReadMode(binary, symbol, strs, value) == true)
+		type[0] = 'R'; // v
+
 	else if (isNote(binary, symbol, strs, value) == true)
 		type[0] = 'n'; // v
 
-	else if (isDebug(binary, symbol, strs, value) == true && value == 64)
+	else if (isDebug(binary, symbol, strs, value) == true)
 		type[0] = 'N'; // x
-
-	else if (isReadMode(binary, symbol, strs, value) == true)
-		type[0] = 'R'; // v
-	// the symbol is in a read-only section
-	// = any section with allocated without the flag SHF_WRITE
-	// = and the section must have the flag SHF_EXECINSTR
 
 	else if (isText(binary, symbol, strs, value) == true)
 		type[0] = 'T'; // v
+
+	else if (isUndefinedWeak(symbol, strs, value) == true)
+		type[0] = 'u'; // v
 
 	else if (isUndefined(symbol, strs, value) == true)
 		type[0] = 'U'; // v
@@ -296,53 +306,9 @@ char*	getType(const char* binary, tSymbols* symbol, tStrs* strs, const int value
 	else if (isWeakUnknown(symbol, strs, value) == true)
 		type[0] = 'W'; // v
 
-	if (isLocalOrGlobal(type[0]) == true && isLocal(symbol, strs, value) == true)
+	if (isLocalOrGlobal(type[0]) == true \
+		&& isGlobal(symbol, strs, value) == false)
 		type[0] += 32; // v
 
 	return (type);
 }
-
-	// A
-	// // "A" The symbol's value is absolute, and  will not be changed by further linking.
-
-	// B
-	// // The symbol is in the BSS data section. This section typically contains zero-initialized
-	// // or uninitialized data, although the exact behavior is system dependent.
-
-	// C
-	// The symbol is common. Common symbols are uninitialized data.
-	// When linking, multiple common symbols may appear with the same
-	// name. If the symbol is defined anywhere, the common symbols
-	// are treated as undefined references.
-
-	// D
-	// // The symbol is in the initialized data section.
-
-	// N
-	// The symbol is a debugging symbol.
-
-	// R
-	// The symbol is in a read only data section.
-
-	// T
-	// The symbol is in the text (code) section.
-
-	// U
-	// The symbol is undefined.
-
-	// V
-	// The symbol is a weak object. When a weak defined symbol is
-	// linked with a normal defined symbol, the normal defined symbol
-	// is used with no error. When a weak undefined symbol is linked
-	// and the symbol is not defined, the value of the weak symbol
-	// becomes zero with no error. On some systems, uppercase
-	// indicates that a default value has been specified.
-
-	// W
-	// The symbol is a weak symbol that has not been specifically
-	// tagged as a weak object symbol. When a weak defined symbol is
-	// linked with a normal defined symbol, the normal defined symbol
-	// is used with no error. When a weak undefined symbol is linked
-	// and the symbol is not defined, the value of the symbol is
-	// determined in a system-specific manner without error. On some
-	// systems, uppercase indicates that a default value has been specified.
